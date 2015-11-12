87ffc6baecf056e3d51f41f3e4ec07b681aa6497
Kate Wood <katewood611@gmail.com>
2015-11-12 14:21:21 -0800

    adds user_routes file

---
+++ routes/user_routes.js
@@ -0,0 +1,14 @@
+var usersController = require('../controllers/users_controller.js'),
+	express			= require('express'),
+	userRoutes		= express.Router() 
+
+//create routes for /users
+userRoutes.route('/')
+	.get(usersController.index)
+	.post(usersController.create)
+
+//create routes for /users/:email
+userRoutes.route('/:email')
+	.get(usersController.show)
+
+module.exports = userRoutes
